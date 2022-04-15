57d818bc433af3b354bcddf51e33e625e94cf528
rl33-glitch <rosslowe30@gmail.com>
2022-04-15 08:55:17 +0000

    add variables for game options

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+//variables for game options
+
+let rockPlayer = document.querySelector(".rock");
+let paperPlayer = document.querySelector(".paper");
+let scissorsPlayer = document.querySelector(".scissors");
+let lizardPlayer = document.querySelector(".lizard");
+let spockPlayer = document.querySelector(".spock");
