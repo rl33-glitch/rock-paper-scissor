719ac18ea909c178267705f69b80d6afca8dc2b8
rl33-glitch <rosslowe30@gmail.com>
2022-04-15 09:06:56 +0000

    add evemt listener for rock option and create a function which replaced image

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+
 //variables for game options
 
 let rockPlayer = document.querySelector(".rock");
@@ -5,3 +6,15 @@ let paperPlayer = document.querySelector(".paper");
 let scissorsPlayer = document.querySelector(".scissors");
 let lizardPlayer = document.querySelector(".lizard");
 let spockPlayer = document.querySelector(".spock");
+
+//variable for player option image
+let userChoiceImg;
+
+// rock option move
+
+rockPlayer.addEventListener("click", rockSelected);
+
+function rockSelected() {
+    userChoiceImg = document.querySelector(".player-selected-img").src = "images/rock-player.png";
+
+}
